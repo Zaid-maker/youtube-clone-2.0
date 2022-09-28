1a01431fa98ce7645348273ec7de8e0a71ebed6e
Zaid-maker <pzhafeez@gmail.com>
2022-09-28 18:12:39 +0500

    Navbar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,46 @@
+import { IconButton, Paper } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const onSubmitHandle = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm("");
+    }
+  };
+
+  return (
+    <Paper
+      component="form"
+      onSubmit={onSubmitHandle}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
+        mr: {
+          sm: 5,
+        },
+      }}
+    >
+      <input className="search-bar" placeholder="Search..." />
+      <IconButton
+        type="submit"
+        sx={{ p: "10px", color: "red" }}
+        aria-label="search"
+      >
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
---
+++ src/utils/constants.js
@@ -0,0 +1,1 @@
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png'
