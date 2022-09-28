40f0fcebbff5a08f380bc79fab6c1654156bf824
DevMirza <pzhafeez@gmail.com>
2022-09-28 17:42:10 +0000

    add styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 import { categories } from '../utils/constants'
 
-const Sidebar = ({ selectedCategory, setSlectedCategory }) => {
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => {
   <Stack
     direction="row"
     sx={{
@@ -13,9 +13,13 @@ const Sidebar = ({ selectedCategory, setSlectedCategory }) => {
     }}
   >
     {categories.map((category) => (
-      <button className="category-btn" key={category.name}>
-        <span>{category.icon}</span>
-        <span>{category.name}</span>
+      <button className="category-btn" onClick={() => setSelectedCategory(category.name)} style={{
+        background: category.name === selectedCategory && "#FC1503"
+      }} key={category.name}>
+        <span style={{
+          color: category.name === selectedCategory ? "white" : "red", marginRight: "15px"
+        }}>{category.icon}</span>
+        <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>{category.name}</span>
       </button>
     ))}
   </Stack>
