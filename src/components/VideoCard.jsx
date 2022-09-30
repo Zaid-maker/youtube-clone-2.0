c1daf889163cc4b6e2daa99cf33c2c8bfa34c7e3
Zaid-maker <pzhafeez@gmail.com>
2022-09-30 16:49:11 +0500

    fix

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,15 +1,22 @@
 import { Card, CardContent, CardMedia, Typography } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import CheckCircleIcon from '@mui/icons-material/CheckCircle'
 
-import { demoChannelTitle, demoChannelUrl, demoThumbnailUrl, demoVideoUrl } from '../utils/constants'
+import {
+  demoChannelTitle,
+  demoChannelUrl,
+  demoThumbnailUrl,
+  demoVideoTitle,
+  demoVideoUrl,
+} from '../utils/constants'
 
 const VideoCard = ({
   video: {
     id: { videoId },
     snippet,
   },
-}) => {
+}) => (
   <Card
     sx={{
       width: { xs: '100%', sm: '358px', md: '320px' },
@@ -24,19 +31,26 @@ const VideoCard = ({
         sx={{ width: { xs: '100%', sm: '358px' }, height: 180 }}
       />
     </Link>
-    <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px' }}>
+    <CardContent sx={{ backgroundColor: '#1E1E1E', height: '106px' }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-        <Typography variant="subtitle1" fontWeight="bold" color="#fff">
-          {snippet?.title.slice(0, 60) || demoVideoUrl.slice(0, 60)}
+        <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+          {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
         </Typography>
       </Link>
-      <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
-        <Typography variant='subtitle2' color="gray">
+      <Link
+        to={
+          snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl
+        }
+      >
+        <Typography variant="subtitle2" color="gray">
           {snippet?.channelTitle || demoChannelTitle}
+          <CheckCircleIcon
+            sx={{ fontSize: '12px', color: 'gray', ml: '5px' }}
+          />
         </Typography>
       </Link>
     </CardContent>
   </Card>
-}
+)
 
 export default VideoCard
