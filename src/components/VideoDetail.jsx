a4bd03e14b600f2a51b7b1b9b28c5ecbde166273
Zaid-maker <pzhafeez@gmail.com>
2022-09-30 17:15:37 +0500

    damn

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { Box, Stack } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import ReactPlayer from 'react-player'
+import { useParams } from 'react-router-dom'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+import { Loader } from './'
 
 const VideoDetail = () => {
-  return <div>VideoDetail</div>;
-};
+  const [videoDetail, setVideoDetail] = useState(null)
+  const [videos, setVideos] = useState(null)
+  const { id } = useParams()
 
-export default VideoDetail;
+  useEffect(() => {
+    fetchFromAPI(`videos?part=anippet.statistics&id=${id}`).then((data) =>
+      setVideoDetail(data.items[0])
+    )
+
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
+      (data) => setVideos(data.items)
+    )
+  }, [id])
+
+  if (!videoDetail?.snippet) return <Loader />
+
+  const {
+    snippet: { title, channelId, channelTitle },
+    statistics: { viewCount, likeCount },
+  } = videoDetail
+
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{ sx: 'column', md: 'row' }}>
+        <Box flex={2}>
+          <Box>
+            <ReactPlayer url={`https://wwww.youtube.com/watch?v=${id}`} className="react-player" controls />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
+  )
+}
+
+export default VideoDetail
